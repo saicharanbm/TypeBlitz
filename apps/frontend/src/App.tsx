c5786cac3ceec0e16e13912f2bf44ccf109c25c5
saicharanbm <saicharanbm@gmail.com>
2025-01-21 17:32:28 +0530

    feat(create-turbo): implement typing game with random words and game state management

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,24 +1,324 @@
+import { Play } from "lucide-react";
+import { useRef, useState, useCallback } from "react";
+interface GameState {
+  words: string[];
+  currentWordIndex: number;
+  currentLetterIndex: number;
+  gameStatus: "waiting" | "playing" | "finished";
+  timeLeft: number;
+  wpm: number;
+}
+
+const words = [
+  "the",
+  "be",
+  "of",
+  "and",
+  "a",
+  "to",
+  "in",
+  "he",
+  "have",
+  "it",
+  "that",
+  "for",
+  "they",
+  "I",
+  "with",
+  "as",
+  "not",
+  "on",
+  "she",
+  "at",
+  "by",
+  "this",
+  "we",
+  "you",
+  "do",
+  "but",
+  "from",
+  "or",
+  "which",
+  "one",
+  "would",
+  "all",
+  "will",
+  "there",
+  "say",
+  "who",
+  "make",
+  "when",
+  "can",
+  "more",
+  "if",
+  "no",
+  "man",
+  "out",
+  "other",
+  "so",
+  "what",
+  "time",
+  "up",
+  "go",
+  "about",
+  "than",
+  "into",
+  "could",
+  "state",
+  "only",
+  "new",
+  "year",
+  "some",
+  "take",
+  "come",
+  "these",
+  "know",
+  "see",
+  "use",
+  "get",
+  "like",
+  "then",
+  "first",
+  "any",
+  "work",
+  "now",
+  "may",
+  "such",
+  "give",
+  "over",
+  "think",
+  "most",
+  "even",
+  "find",
+  "day",
+  "also",
+  "after",
+  "way",
+  "many",
+  "must",
+  "look",
+  "before",
+  "great",
+  "back",
+  "through",
+  "long",
+  "where",
+  "much",
+  "should",
+  "well",
+  "people",
+  "down",
+  "own",
+  "just",
+  "because",
+  "good",
+  "each",
+  "those",
+  "feel",
+  "seem",
+  "how",
+  "high",
+  "too",
+  "place",
+  "little",
+  "world",
+  "very",
+  "still",
+  "nation",
+  "hand",
+  "old",
+  "life",
+  "tell",
+  "write",
+  "become",
+  "here",
+  "show",
+  "house",
+  "both",
+  "between",
+  "need",
+  "mean",
+  "call",
+  "develop",
+  "under",
+  "last",
+  "right",
+  "move",
+  "thing",
+  "general",
+  "school",
+  "never",
+  "same",
+  "another",
+  "begin",
+  "while",
+  "number",
+  "part",
+  "turn",
+  "real",
+  "leave",
+  "might",
+  "want",
+  "point",
+  "form",
+  "off",
+  "child",
+  "few",
+  "small",
+  "since",
+  "against",
+  "ask",
+  "late",
+  "home",
+  "interest",
+  "large",
+  "person",
+  "end",
+  "open",
+  "public",
+  "follow",
+  "during",
+  "present",
+  "without",
+  "again",
+  "hold",
+  "govern",
+  "around",
+  "possible",
+  "head",
+  "consider",
+  "word",
+  "program",
+  "problem",
+  "however",
+  "lead",
+  "system",
+  "set",
+  "order",
+  "eye",
+  "plan",
+  "run",
+  "keep",
+  "face",
+  "fact",
+  "group",
+  "play",
+  "stand",
+  "increase",
+  "early",
+  "course",
+  "change",
+  "help",
+  "line",
+];
+
+interface WordElementRef extends HTMLDivElement {
+  children: HTMLCollectionOf<LetterElement>;
+}
+interface LetterElement extends HTMLSpanElement {
+  className: string;
+  textContent: string;
+}
 function App() {
+  const GAME_TIME = useRef<number>(30 * 1000);
+  const [gameState, setGameState] = useState<GameState>({
+    words: [],
+    currentWordIndex: 0,
+    currentLetterIndex: 0,
+    gameStatus: "waiting",
+    timeLeft: GAME_TIME.current / 1000,
+    wpm: 0,
+  });
+  const wordsRef = useRef<HTMLDivElement | null>(null);
+  const gameRef = useRef<HTMLDivElement | null>(null);
+
+  const getRandomWord = (): string => {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+  };
+
+  const initializeGame = useCallback((): void => {
+    const newWords = Array.from({ length: 200 }, () => getRandomWord());
+    setGameState((prev) => ({
+      ...prev,
+      words: newWords,
+      currentWordIndex: 0,
+      currentLetterIndex: 0,
+      gameStatus: "waiting",
+      timeLeft: GAME_TIME.current / 1000,
+      wpm: 0,
+    }));
+  }, []);
+
+  const getCursorPosition = () => {
+    if (!wordsRef.current) return { top: 0, left: 0 };
+
+    const currentWord = wordsRef.current.children[
+      gameState.currentWordIndex
+    ] as WordElementRef | undefined;
+    if (!currentWord) return { top: 0, left: 0 };
+
+    const currentLetter = currentWord.children[gameState.currentLetterIndex];
+    const rect = currentLetter
+      ? currentLetter.getBoundingClientRect()
+      : currentWord.getBoundingClientRect();
+
+    return {
+      top: rect.top + 2,
+      left: currentLetter ? rect.left : rect.right,
+    };
+  };
   return (
-    <>
-      <h1>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-          <path d="M584.2 96.36c-28.88-1.701-54.71 17.02-79.74 26.49C490 88.22 455.9 64 416 64c-11.25 0-22 2.252-32 5.877V56C384 42.75 373.2 32 360 32h-16C330.8 32 320 42.75 320 56v49C285.1 79.62 241.2 64 192 64C85.1 64 0 135.6 0 224v232C0 469.3 10.75 480 24 480h48C85.25 480 96 469.3 96 456v-62.87C128.4 407.5 166.8 416 208 416s79.63-8.492 112-22.87V456c0 13.25 10.75 24 24 24h48c13.25 0 24-10.75 24-24V288h128v32c0 8.837 7.163 16 16 16h32c8.837 0 16-7.163 16-16V288c17.62 0 32-14.38 32-32l-.0001-96.07C639.1 127.8 616.4 98.25 584.2 96.36zM447.1 176c-8.875 0-16-7.125-16-16S439.1 144 448 144s16 7.125 16 16S456.9 176 447.1 176z" />
-        </svg>
-        HippoType
-      </h1>
-      <div id="header">
-        <div id="info"></div>
-        <div id="buttons">
-          <button id="newGameBtn">New game</button>
+    <div className="max-w-[1400px] min h-screen mx-auto pt-12  px-4 sm:px-6 lg:px-12 ">
+      <nav>
+        <h1 className="text-2xl md:text-3xl  lg:text-4xl  text-textPrimary flex items-center gap-2 mb-8">
+          <Play className="w-8 h-8 text-primaryColor" />
+          TypeBlitz
+        </h1>
+      </nav>
+
+      <div className="flex justify-between items-center mb-8">
+        <div className="text-yellow-400 text-xl">
+          {gameState.gameStatus === "finished"
+            ? `WPM: ${gameState.wpm}`
+            : gameState.timeLeft}
         </div>
+        <button
+          onClick={initializeGame}
+          className="bg-white/20 text-white/50 px-5 py-1.5 rounded hover:bg-white/30 transition-colors"
+        >
+          New game
+        </button>
       </div>
-      <div id="game">
-        <div id="words"></div>
-        <div id="cursor"></div>
-        <div id="focus-error">Click here to focus</div>
+      <div
+        ref={gameRef}
+        className={`relative h-[105px] overflow-hidden leading-9 focus:outline-none ${
+          gameState.gameStatus === "finished" ? "opacity-50" : ""
+        }`}
+        tabIndex={0}
+      >
+        <div ref={wordsRef} className={`text-gray-500 `}>
+          {gameState.words.map((word, wordIndex) => (
+            <div key={wordIndex} className="inline-block font-mono mx-1">
+              {word.split("").map((letter, letterIndex) => (
+                <span key={letterIndex} className="letter">
+                  {letter}
+                </span>
+              ))}
+            </div>
+          ))}
+        </div>
+        {gameState.gameStatus !== "finished" && (
+          <div
+            className="w-0.5 h-6 bg-yellow-400 fixed animate-[blink_0.3s_infinite]"
+            style={{
+              top: `${getCursorPosition().top}px`,
+              left: `${getCursorPosition().left}px`,
+            }}
+          />
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: "var(--bgColor)",
+        textPrimary: "var(--textPrimary)",
+        textSecondary: "var(--textSecondary)",
+        primaryColor: "var(--primaryColor)",
+      },
+    },
   },
   plugins: [],
 };
